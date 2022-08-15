693b1b4424595bdee394d0087f27f380502acb90
Asif <49162687+nhAsif@users.noreply.github.com>
2022-08-15 12:25:06 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+mkdir Gapps
+cd Gapps
+git clone --depth=1 https://github.com/nhAsif/build_nikgapps.git -b main build
+git clone --depth=1 https://github.com/nhAsif/config.git -b main config
 sudo apt install aapt -y
-sudo apt update
 sudo apt install python3 -y
 python3 -m pip install wheel setuptools testresources
 pip3 install --upgrade pip
-git clone --depth=1 https://github.com/nhAsif/build_nikgapps.git -b main build
-git clone --depth=1 https://github.com/nhAsif/config.git -b main config
 pip3 install -r build/requirements.txt
 read -p "Enter your android version: " adv
 wget https://raw.githubusercontent.com/nhAsif/config/main/12.1/nikgapps.config config/$adv
